2b6bf516795de6c46c015940fd96d6a6740bcd99
Sanu Kumar <sanu.kumar@example.com>
2026-02-17 12:49:51 +0530

    Enable static export for Render deployment

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+}
+
+module.exports = nextConfig
